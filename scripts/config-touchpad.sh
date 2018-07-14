3e3b751c2e242fe7cc9ac63b40829e20c4af62ee
Rodrigo Campos <rod.apd@gmail.com>
2018-07-14 09:50:01 -0300

    Add lightdm as display manager

---
+++ scripts/config-touchpad.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TOUCHPAD=$(xinput | grep -i -m1 "touchpad" | pcregrep -o1 '=([0-9]+)')
+NATURAL=$(xinput list-props $TOUCHPAD | grep -i -m1 "natural" | pcregrep -o1 '\(([0-9]+)\)')
+TAP=$(xinput list-props $TOUCHPAD | grep -i -m1 "tap" | pcregrep -o1 '\(([0-9]+)\)')
+TAPDRAG=$(xinput list-props $TOUCHPAD | grep -i -m1 "tap.*drag" | pcregrep -o1 '\(([0-9]+)\)')
+# natural scrolling
+xinput set-prop $TOUCHPAD $NATURAL 1
+# tap to clik
+xinput set-prop $TOUCHPAD $TAP 1
+# stop tap and drag
+xinput set-prop $TOUCHPAD $TAPDRAG 0
