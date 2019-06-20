60e784992401e80e7203fa85e8eee36f2daf381a
Rodrigo Campos <rod.apd@gmail.com>
2019-06-19 23:40:52 -0300

    Add autostart running checks

--- .autostart.sh
+++ .autostart.sh
@@ -6,14 +6,26 @@ source ~/.profile
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 export SSH_AUTH_SOCK
 
-dunst &
+if ! pgrep -x "dunst" > /dev/null; then
+  dunst &
+fi
+if ! pgrep -x "unclutter" > /dev/null; then
+  unclutter -b &
+fi
+if ! pgrep -x "greenclip" > /dev/null; then
+  greenclip daemon &
+fi
+if ! pgrep -x "compton" > /dev/null; then
+  compton -f -b &
+fi
+if ! pgrep -x "urxvtd" > /dev/null; then
+  urxvtd -q -o -f &
+fi
+
 xrdb -load ~/.Xresources &
-unclutter -b &
-greenclip daemon &
-compton -f -b &
-urxvtd -q -o -f &
+xmodmap ~/.Xmodmap
 setxkbmap -option 'caps:ctrl_modifier'
 xcape -t 200 -e 'Caps_Lock=Escape'
-setxkbmap br &
+setxkbmap br
 
 
