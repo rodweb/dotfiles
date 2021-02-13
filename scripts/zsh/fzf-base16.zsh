3b7a462cebb77dba8d40ea0cc0f0a51c98efeef5
Rodrigo Campos <rod.apd@gmail.com>
2021-02-13 11:06:22 -0300

    remove scripts directory

--- scripts/zsh/fzf-base16.zsh
+++
@@ -1,6 +0,0 @@
-# fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
-fcs() {
-  local cs
-  cs=$(ls -1 ~/.config/base16-shell/scripts | sed -e 's/\..*$//' | sed -e 's/base16-/base16_/') &&
-          eval $(echo $cs | fzf)
-}
