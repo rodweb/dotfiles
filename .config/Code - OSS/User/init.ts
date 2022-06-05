881d52e220d261e88a3275b3918a823e831468dd
Rodrigo Campos <rod.apd@gmail.com>
2022-06-05 12:31:15 -0300

    feat(vscode): update bindings and settings

--- .config/Code - OSS/User/init.ts
+++ .config/Code - OSS/User/init.ts
@@ -1,7 +1,6 @@
 import * as vscode from "vscode";
 
 export function init(context: vscode.ExtensionContext) {
-    vscode.window.showInformationMessage('Initialized');
     const config = vscode.workspace.getConfiguration();
     config.update("workbench.editor.showTabs", false, vscode.ConfigurationTarget.Global);
     config.update("editor.minimap.enabled", false, vscode.ConfigurationTarget.Global);
